d3b68d9ce97028161cec2638d2cf2e60edbe1248
tarun6546 <tarunvarshney2112@gmail.com>
2025-05-22 23:30:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const connectDb = require('./config/connectDb');
+
+//env
+dotenv.config();
+
+//databasecall
+connectDb();
+
+
+//object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.get('/', (req, res) => {
+  res.send('<h1>Tarun Varshney</h1>');
+});
+
+//ports
+const PORT = process.env.PORT || 8080;
+
+//server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
