2c20b3331969fd0d3f1d8df93f8e25e55bc91899
tarun6546 <tarunvarshney2112@gmail.com>
2025-05-23 23:57:19 +0530

    feat: Initialize React application with basic structure and components
    - Added logo512.png for application branding.
    - Created manifest.json for PWA configuration.
    - Added robots.txt for search engine directives.
    - Implemented App component with a simple header.
    - Created App.test.js for initial testing setup.
    - Developed Footer and Header components for layout.
    - Established Layout component to wrap content with Header and Footer.
    - Added global CSS reset in index.css.
    - Set up index.js for rendering the main App component.
    - Included reportWebVitals.js for performance measurement.
    - Configured setupTests.js for testing environment.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center">Tarun Varshney</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bd-dark text-light'>
+        <h6 className='text-center'>ALL RIGHTS RESERVED </h6>
+
+    </div>
+  )
+}
+
+export default Footer
